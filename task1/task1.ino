9e1fc2a87d40b5c4821e67a06910a90e5e68e217
Aleks_Rock <ringo212005@gmail.com>
2024-02-13 13:14:28 +0200

    add task1

---
+++ task1/task1.ino
@@ -0,0 +1,12 @@
+const String command = "AT+";
+const int pause = 500; // msec
+byte count = 0;
+void setup() {
+ Serial.begin(9600);
+}
+void loop() {
+ Serial.print(command);
+ Serial.print(count++);
+ Serial.print("\r\n");
+ delay(pause);
+}
